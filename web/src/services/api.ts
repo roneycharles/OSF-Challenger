bba344b3e20c130298bdc3c59a2d1fde598011f5
Roney Charles Monteiro Chaves <mmn.charles@gmail.com>
2020-11-09 19:59:58 -0300

    add new api url

--- web/src/services/api.ts
+++ web/src/services/api.ts
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 const api = axios.create({
   // baseURL: 'https://api.github.com/',
-  // baseURL: 'http://osf-challenger.herokuapp.com/',
-  baseURL: 'http://localhost:8080/',
+  baseURL: 'https://backend-osf.herokuapp.com/',
+  // baseURL: 'http://localhost:8080/',
 });
 
 export default api;
